c16f3894da307a87e9ceccf2696311d0791c626c
folt3k <folt3k@gmail.com>
2023-04-04 18:21:33 +0200

    inir prisma

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from 'dotenv';
+import prisma from "../prisma/client";
 
 dotenv.config();
 
@@ -11,7 +12,9 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/',async  (req: Request, res: Response) => {
+    const x = await prisma.get('images');
+    console.log(x)
     res.send('Express + TypeScript Server');
 });
 
