385e711283bd2b2db4e743210d375a3e83844876
kfoltyniak <kfoltyniak@software.altkom.pl>
2023-04-10 18:56:42 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -22,5 +22,5 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running1 at http://localhost:${port}`);
 });
